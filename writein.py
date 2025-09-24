80c4eb19398db8eb9191cb9038183fa62bae10b6
WANGICHEN <149982770+WANGICHEN@users.noreply.github.com>
2025-09-24 22:03:46 +0800

    Update writein.py

--- writein.py
+++ writein.py
@@ -5,9 +5,8 @@ import io
 import os
 from docx.shared import RGBColor
 
-def write_doc(word_path, info):
+def write_doc(doc, info):
 
-    doc = Document(word_path)
     for para in doc.paragraphs:
         full_text = ''.join(run.text for run in para.runs)
         replaced = False
@@ -53,17 +52,29 @@ def create_zip(file_dict):
 
 
 
-def run_BSMI_doc(word_path, info):
+def run_BSMI_doc(info):
     print("run BSMI doc")
     files = {}
-    fs = glob(word_path + '*.docx')
+    fs = ["https://z28856673-my.sharepoint.com/:w:/g/personal/itek_project_i-tek_com_tw/EddnQOquWcRFnEg7TWvy2r0BPAxZon_0AgUEMR8wygTOfA?e=llylt7",
+         "https://z28856673-my.sharepoint.com/:w:/g/personal/itek_project_i-tek_com_tw/EZRXXI9yXRhJuZ1o1WV1iOIBAeD36nOTZe5Ojo5hl7hpmw?e=aKc3ZZ",
+         "https://z28856673-my.sharepoint.com/:w:/g/personal/itek_project_i-tek_com_tw/EQk0sg6ngDxHhPE0pO894Q4BZnkPKc0Y1qYehDwvYPQCdQ?e=leuclc",
+         "https://z28856673-my.sharepoint.com/:w:/g/personal/itek_project_i-tek_com_tw/Ea3dOFrVSFlMtLBljbBtW4oBfJO9g7z8xY8pWIOhA5H-gg?e=V0i1zO",
+         "https://z28856673-my.sharepoint.com/:w:/g/personal/itek_project_i-tek_com_tw/ESYvqMkqG9NBlqzSQJQHLWgB9NoCWQLJiPWm-lYVU_pEbQ?e=EeMRfu"]
+    
     for f in fs:
-        doc = write_doc(f, info)
+        download_url = share_url + ("&download=1" if "?" in share_url else "?download=1")
+            
+        headers = {"User-Agent": "Mozilla/5.0"}
+        r = requests.get(download_url, headers=headers, allow_redirects=True, timeout=30)
+        r.raise_for_status()  # 403/404 會在這裡丟錯
+        
+        doc = write_doc(Document(BytesIO(r.content)), info)
         buf = io.BytesIO()
         doc.save(buf)
         buf.seek(0)
         files[os.path.basename(f)] = buf.read()
 
     zip_buffer = create_zip(files)
+
 
     return zip_buffer
